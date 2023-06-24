8b700f9794a59c8e7add80964affb1fe4aa34e53
dominik49526 <137609759+dominik49526@users.noreply.github.com>
2023-06-24 12:35:16 +0200

    skrypt1.sh

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,1 +1,13 @@
-date
+#
+
+wyswietl_date=false
+
+for flagi in "$@"; do
+    case $flagi in
+        --date) show_date=true ;;
+    esac
+done
+
+if $wyswietl_date; then
+    aktualna_data=date
+    echo "Aktualna data: $aktualna_data"
