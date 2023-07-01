19c2e70309413f93fc4eae31a476b4cc05c39704
Dominik Nasiłowski <dsw49526@student.dsw.edu.pl>
2023-07-01 10:10:23 +0200

    skrypt.sh

--- skrypt1.sh
+++ skrypt1.sh
@@ -2,11 +2,13 @@
 
 wyswietl_date=false
 sto_logow=false
+pomoc=false
 
 for flagi in "$@"; do
     case $flagi in
         --date) wyswietl_date=true ;;
         --logs) sto_logow=true ;;
+        --help) pomoc=true
     esac
 done
 
@@ -21,6 +23,13 @@ if $sto_logow; then
         nazwa_pliku="log${i}.txt"
         echo "$nazwa_pliku $(date)" > "$nazwa_pliku"
     done
+fi
+
+if $pomoc; then
+    echo "Opcje:"
+    echo " - skrypt.sh --date : Wyświetla dzisiejszą datę."
+    echo " - skrypt.sh --logs [liczba] : Tworzy 100 plików tekstowych "
+    echo " - skrypt.sh --help : Wyświetla wszystkie dostępne opcje."
 fi
 
 touch .gitignore
